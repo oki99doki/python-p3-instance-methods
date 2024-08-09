328a50edc238c66f3c2a8360b80b49693dcc69be
Stefan Klinkert <s.klinkert@gmail.com>
2024-08-09 07:32:56 +0200

    complete instance lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 
+import ipdb
+
 class Dog:
     # Class body goes here
 
     #Instance method definition
-    pass
+    #pass
+    def bark(self):
+        print("Woof!")
+    
+    def sit(self):
+        print("The dog is sitting.")
+
+fido = Dog()
+fido.bark()
+
+snoopy = Dog()
+snoopy.bark()
+
+#bark()
+
+#fido.sit()
+
+#ipdb.set_trace()
